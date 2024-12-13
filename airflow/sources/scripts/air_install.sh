93266b4148cbbaf4663f2a5f1dd51d2f67a121a4
pengfei <liu.pengfei@hotmail.fr>
2024-12-13 15:14:11 +0100

    update installation script

--- airflow/sources/scripts/air_install.sh
+++ airflow/sources/scripts/air_install.sh
@@ -2,16 +2,23 @@
 
 # set up the home folder for airflow application, it will store all the required files of the airflow
 # if not define, when you run the server, it will create the files in ~/airflow
-air_root_path=/home/pengfei/opt/airflow
+air_root_path=~/opt/airflow
 
 # set up python and airflow version
 AIRFLOW_VERSION=2.9.2
 PYTHON_VERSION="$(python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')"
 
-# add this to your bashrc
-echo 'AIRFLOW_HOME=$air_root_path' >> ~/.bashrc
+# Add AIRFLOW_HOME to your bashrc if not already present
+if ! grep -q "AIRFLOW_HOME=$air_root_path" ~/.bashrc; then
+  echo "export AIRFLOW_HOME=\"$air_root_path\"" >> ~/.bashrc
+  echo "AIRFLOW_HOME added to ~/.bashrc"
+else
+  echo "AIRFLOW_HOME is already set in ~/.bashrc"
+fi
 
-export AIRFLOW_HOME=$air_root_path
+# Export the environment variable for the current session
+export AIRFLOW_HOME="$air_root_path"
+echo "AIRFLOW_HOME is set to $AIRFLOW_HOME"
 
 # auto determine the constraint url
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"

316ff7c6435ac9881fa5a3ca954ca30af742583e
pengfei liu <liu.pengfei@hotmail.fr>
2025-10-23 12:10:13 +0200

    add flows

---
+++ prefect/server_conf/flows/pengfei/simple_flow.py
@@ -0,0 +1,29 @@
+from prefect import flow, task
+
+@task(log_prints=True)
+def extract():
+    data = [1, 2, 3]
+    print(f"Task1: Extracting data: {data}")
+    return data
+
+@task(log_prints=True)
+def transform(data):
+    print(f"Task2: Transforming data: {data}")
+    return [x * 10 for x in data]
+
+@task(log_prints=True)
+def load(data):
+    print(f"Task3: Loading result {data}")
+
+@flow
+def etl_flow():
+    # task 1
+    data = extract()
+    # task 2
+    clean = transform(data)
+    # task 3
+    load(clean)
+
+
+if __name__ == "__main__":
+    etl_flow()
